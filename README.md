7eae7bd5fcff63b2f59efeb889b4092c9a2b3bde
Chaz Henricks <chazhenricks@gmail.com>
2017-04-19 11:31:14 -0500

    Messin with MARKDOWN

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-#Dynamic Cards
+
+# Dynamic Cards
+
+1. Using Javascript to create cards once the button is clicked.
+1. Each button will also create a delete button that removes the created card.
--- cards.js
+++ cards.js
@@ -3,9 +3,16 @@ var inputButton = document.getElementById("button");
 var container = document.getElementById("container");
 
 
+
 inputButton.addEventListener("click", function(event){
     container.innerHTML +=
         `<div class="card">
             <button type="button" class ="delete">DELETE</button>
         </div>`;
+        changeClass();
+
 });
+
+
+
+
